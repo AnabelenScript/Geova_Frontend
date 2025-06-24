23b8a0b34c2e3bcd845a0b326b74b08281a6ad8c
AnabelenScript <ana0507belen@gmail.com>
2025-06-23 23:28:41 -0600

    Organización de carpetas con MVM y comienzo de Login

---
+++ src/views/Login/LoginForm.tsx
@@ -0,0 +1,16 @@
+import './LoginForm.css'
+
+function Login(){
+    return(
+        <>
+        <div className="LoginContainer">
+            <div className="LoginForm">
+                <div></div>
+                <div></div>
+            </div>
+        </div>
+        </>
+    )
+}
+
+export default Login
